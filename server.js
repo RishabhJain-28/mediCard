57f9c850aa69a222fb07d572eac466b141bbf611
akgupta-47 <akshatgupta3214@gmail.com>
2022-03-09 17:52:56 +0530

    auth added

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+
+const passport = require("passport");
+const MongoStore = require("connect-mongo");
+const { ensureAuth, ensureGuest } = require("./middleware/auth");
+
 const path = require("path");
-const envPath = path.join(
-  __dirname,
-  process.env.NODE_ENV === "development" ? "/dev.env" : "/.env"
-);
-require("dotenv").config({ path: envPath });
+// const envPath = path.join(
+//     __dirname,
+//     process.env.NODE_ENV === "development" ? "/dev.env" : "/.env"
+// );
+
+require("dotenv").config({ path: "./dev.env" });
 const cors = require("cors");
 const connectDB = require("./config/db");
 
@@ -16,24 +24,41 @@ const app = express();
 
 app.use(express.json());
 app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    })
 );
+
+require("./config/passport")(passport);
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/auth", require("./routes/auth"));
+
 app.use("/api/record", record);
 app.use("/", (req, res) => {
-  res.send("heelo");
+    res.send("heelo");
 });
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, console.log(`Server started on Port ${PORT}`));
 
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
-  server.close(() => process.exit(1));
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1));
 });
 process.on("uncaughtException", (err, promise) => {
-  console.log(`Error: ${err.message}`);
-  server.close(() => process.exit(1));
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1));
 });
